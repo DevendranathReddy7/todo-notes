867dd9f04349f6c9b23f9f4d282e333de45dc511
Devendra <devendranathreddy54@gmail.com>
2023-10-13 21:50:24 +0530

    updated bg color

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Notes from './components/Notes';
 import ToDo from './components/ToDo';
 import store from './store';
 import { Provider } from 'react-redux';
-
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
