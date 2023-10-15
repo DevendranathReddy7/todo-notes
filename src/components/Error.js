140c4eeb2e9ea7fc088bd82f7ac5520f7fd86cc8
Devendra <devendranathreddy54@gmail.com>
2023-10-15 20:28:45 +0530

    updated tooltip info and app height

--- src/components/Error.js
+++ src/components/Error.js
@@ -10,7 +10,7 @@ const Error = (props) => {
                 <div className="wrapper">
                     <div className="error_wrapper1">
                         <span className="tootip span">&#x1F6C8;</span>
-                        <p className="hide">Please add a valid Item or action on the error to add a new Item</p>
+                        <p className="hide">Please add a valid Item or action on the error to remove the Error.</p>
                         <h4 className="err">{props.err}</h4>
                     </div>
                     <div><button className="btn close_btn" onClick={closeHandler}>X</button ></div>
